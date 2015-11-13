9f37898810ef8350117b36091020d3777f671338
Anders Holmgren <aholmgren@atlassian.com>
2015-11-13 16:04:23 +1100

    fix error handling for es 2

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -3,13 +3,21 @@ part of elastic_dart;
 final _indexMissing = new RegExp(r'IndexMissingException\[\[([^\]]+)\] missing\]');
 String _missingIndex(String error) => _indexMissing.firstMatch(error).group(1);
 
+String _errorMessage(responseError) {
+  if (responseError is String) return responseError;
+  else if (responseError is Map) return responseError['reason'];
+  else return null;
+}
+
+
 class ElasticsearchException implements Exception {
   final String message;
   final int status;
   final Map response;
 
   ElasticsearchException(Map response, [String message]) :
-    this.message = (message == null) ? response['error'] : message,
+    this.message = (message == null)
+        ? _errorMessage(response['error']) : message,
     this.status = response['status'],
     this.response = response;
 
